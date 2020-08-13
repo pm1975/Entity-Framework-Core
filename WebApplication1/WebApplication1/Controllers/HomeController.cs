df3e51a0289f2400726fa705352a72976af8eff4
PM <pm1975@windowslive.com>
2020-08-13 15:17:19 +0200

    7. Magia Entity Framework - jak to baza danych bez ani linijki SQL?

--- WebApplication1/WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -5,21 +5,52 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using WebApplication1.Database;
 using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly IServiceProvider mServiceProvider;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(IServiceProvider serviceProvider)
         {
-            _logger = logger;
+            mServiceProvider = serviceProvider;
         }
+
+        //private readonly ILogger<HomeController> _logger;
+
+        //public HomeController(ILogger<HomeController> logger)
+        //{
+        //    _logger = logger;
+        //}
 
         public IActionResult Index()
         {
+            var database = mServiceProvider.GetService(typeof(WebApplication1DbContext)) as WebApplication1DbContext;
+            var settingsTable = database.Settings;
+
+            #region Dodajemy nowe ustawienia tła
+            //var newSetting = new Setting
+            //{
+            //    Name = "Background",
+            //    Value = "Black"
+            //};
+            //settingsTable.Add(newSetting);
+            #endregion
+
+            #region Usuwamy duplikat czarnego tła
+            //var firstBackgroundSetting = settingsTable.Where(x => x.Name == "Background").First();
+            //settingsTable.Remove(firstBackgroundSetting);
+            #endregion
+
+            #region Zmienmy istniejacy background na red
+            var backgroundSetting = settingsTable.First();
+            backgroundSetting.Value = "Red";
+            #endregion
+            database.SaveChanges();
+
             return View();
         }
 
